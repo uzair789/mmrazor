ada67ec2ea067316810b5c947fea468f480cac41
uzair789 <uzaira@andrew.cmu.edu>
2022-05-02 02:33:50 -0400

    [ultron] Distilling with Resnet50D as teacher over subresnet. Student is trained from scratch with a pretrained backbone with imagenet

--- tools/mmdet/dist_train.sh
+++ tools/mmdet/dist_train.sh
@@ -4,7 +4,7 @@ CONFIG=$1
 GPUS=$2
 NNODES=${NNODES:-1}
 NODE_RANK=${NODE_RANK:-0}
-PORT=${PORT:-29500}
+PORT=${PORT:-29505}
 MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 
 PYTHONPATH="$(dirname $0)/../..":$PYTHONPATH \
@@ -14,6 +14,6 @@ python -m torch.distributed.launch \
     --master_addr=$MASTER_ADDR \
     --nproc_per_node=$GPUS \
     --master_port=$PORT \
-    $(dirname "$0")/mmdet/train_mmdet.py \
+    $(dirname "$0")/train_mmdet.py \
     $CONFIG \
     --launcher pytorch ${@:3}
